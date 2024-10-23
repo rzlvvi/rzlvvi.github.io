9a76efdfd76c9283cc0ae887734e61f82d4eb48b
rzlvvi <muhammadrhezapleevi@gmail.com>
2024-10-24 01:25:16 +0700

    second commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html']['./cart.html']['./akun.html']['./login.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
